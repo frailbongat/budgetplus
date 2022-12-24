6921f61982fcd7aa7accd48966ae214fbd62fe91
Frail Bongat <frail.bongat@gmail.com>
2022-12-24 22:17:19 +0800

    Initial Commit

---
+++ budgetplus/budgetplusApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetplusApp.swift
+//  budgetplus
+//
+//  Created by Frail Bongat on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct budgetplusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
